55370043ce2b82b60c68e42bfacf1d9f063b8deb
Jaime Pastor <jaime.pastor@adidas-group.com>
2018-11-19 13:30:50 +0100

    Add javascript challenge

---
+++ adidas-ascii-logo/javascript/src/adidas-ascii-logo.js
@@ -0,0 +1,8 @@
+/**
+ * Returns the adidas three stripes logo using `@` characters.
+ * @param {number} width - Width of a stripe.
+ * @returns {string} adidas logo.
+ */
+module.exports = function(width) {
+  return '';
+};
---
+++ adidas-ascii-logo/javascript/test/specs/adidas-ascii-logo.spec.js
@@ -0,0 +1,111 @@
+const createAdidasAsciiLogo = require('../../src/adidas-ascii-logo');
+
+/**
+ * Trims the given value.
+ * @param {string} value - Value to be trimmed
+ * @returns {string} Trimmed value.
+ */
+function trim(value) {
+  return value.replace(/^\n|\s+$/g, '');
+}
+
+describe('adidas ASCII logo', () => {
+  it('should throw and exception if the width is less than `2`', () => {
+    expect(() => createAdidasAsciiLogo(1)).toThrowError('Error, minimun width is 2');
+  });
+
+  it('should return the adidas logo given a stripe width of `2`', () => {
+    expect(createAdidasAsciiLogo(2)).toEqual(trim(`
+    @@
+  @@ @@
+@@ @@ @@
+    `));
+  });
+
+  it('should return the adidas logo given a stripe width of `3`', () => {
+    expect(createAdidasAsciiLogo(3)).toEqual(trim(`
+      @@@
+       @@@
+   @@@  @@@
+    @@@  @@@
+@@@  @@@  @@@
+ @@@  @@@  @@@
+    `));
+  });
+
+  it('should return the adidas logo given a stripe width of `5`', () => {
+    expect(createAdidasAsciiLogo(5)).toEqual(trim(`
+          @@@@@
+           @@@@@
+     @@@@@  @@@@@
+      @@@@@  @@@@@
+@@@@@  @@@@@  @@@@@
+ @@@@@  @@@@@  @@@@@
+    `));
+  });
+
+  it('should return the adidas logo given a stripe width of `7`', () => {
+    expect(createAdidasAsciiLogo(7)).toEqual(trim(`
+              @@@@@@@
+               @@@@@@@
+                @@@@@@@
+       @@@@@@@   @@@@@@@
+        @@@@@@@   @@@@@@@
+         @@@@@@@   @@@@@@@
+@@@@@@@   @@@@@@@   @@@@@@@
+ @@@@@@@   @@@@@@@   @@@@@@@
+  @@@@@@@   @@@@@@@   @@@@@@@
+    `));
+  });
+
+  it('should return the adidas logo given a stripe width of `9`', () => {
+    expect(createAdidasAsciiLogo(9)).toEqual(trim(`
+                  @@@@@@@@@
+                   @@@@@@@@@
+                    @@@@@@@@@
+         @@@@@@@@@   @@@@@@@@@
+          @@@@@@@@@   @@@@@@@@@
+           @@@@@@@@@   @@@@@@@@@
+@@@@@@@@@   @@@@@@@@@   @@@@@@@@@
+ @@@@@@@@@   @@@@@@@@@   @@@@@@@@@
+  @@@@@@@@@   @@@@@@@@@   @@@@@@@@@
+    `));
+  });
+
+  it('should return the adidas logo given a stripe width of `16`', () => {
+    expect(createAdidasAsciiLogo(16)).toEqual(trim(`
+                                @@@@@@@@@@@@@@@@
+                                 @@@@@@@@@@@@@@@@
+                                  @@@@@@@@@@@@@@@@
+                                   @@@@@@@@@@@@@@@@
+                @@@@@@@@@@@@@@@@    @@@@@@@@@@@@@@@@
+                 @@@@@@@@@@@@@@@@    @@@@@@@@@@@@@@@@
+                  @@@@@@@@@@@@@@@@    @@@@@@@@@@@@@@@@
+                   @@@@@@@@@@@@@@@@    @@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@    @@@@@@@@@@@@@@@@    @@@@@@@@@@@@@@@@
+ @@@@@@@@@@@@@@@@    @@@@@@@@@@@@@@@@    @@@@@@@@@@@@@@@@
+  @@@@@@@@@@@@@@@@    @@@@@@@@@@@@@@@@    @@@@@@@@@@@@@@@@
+   @@@@@@@@@@@@@@@@    @@@@@@@@@@@@@@@@    @@@@@@@@@@@@@@@@
+    `));
+  });
+
+  it('should return the adidas logo given a stripe width of `21`', () => {
+    expect(createAdidasAsciiLogo(21)).toEqual(trim(`
+                                          @@@@@@@@@@@@@@@@@@@@@
+                                           @@@@@@@@@@@@@@@@@@@@@
+                                            @@@@@@@@@@@@@@@@@@@@@
+                                             @@@@@@@@@@@@@@@@@@@@@
+                                              @@@@@@@@@@@@@@@@@@@@@
+                     @@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@
+                      @@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@
+                       @@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@
+                        @@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@
+                         @@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@
+ @@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@
+  @@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@
+   @@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@
+    @@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@
+    `));
+  });
+});
