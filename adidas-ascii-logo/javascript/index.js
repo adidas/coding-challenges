7b5f504951aa89c08376a0dc27f83e5a5835fb0a
Jaime Pastor <jaime.pastor@adidas-group.com>
2018-11-20 10:11:57 +0100

    Fix adidas ascii javascript challenge

--- adidas-ascii-logo/javascript/index.js
+++ adidas-ascii-logo/javascript/index.js
@@ -1,5 +1,7 @@
 const createAdidasAsciiLogo = require('./src/adidas-ascii-logo');
 
 [ 2, 3, 4, 9, 16 ].forEach((width) => {
-  process.stdout.write(`\n${ createAdidasAsciiLogo(width) }\n`);
+  process.stdout.write(`\nadidas (width ${ width })`);
+  process.stdout.write('\n-------------------------------------------------------------');
+  process.stdout.write(`\n${ createAdidasAsciiLogo(width) }\n\n`);
 });
